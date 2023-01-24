2e065a4710a112b8084c472f5b4954a48f4c866b
TemiloluwaPraise <temiloluwapraise06@gmail.com>
2023-01-24 08:23:05 +0100

    changes

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
                 <p className='uppercase pt-8'>Connect With Me</p>
                 <div className='flex items-center justify-between py-4'>
                   <a
-                    href='https://www.https://www.linkedin.com/in/praise-temiloluwa-453392252/'
+                    href='https://www.linkedin.com/in/praise-temiloluwa-453392252/'
                     target='_blank'
                     rel='noreferrer'
                   >
